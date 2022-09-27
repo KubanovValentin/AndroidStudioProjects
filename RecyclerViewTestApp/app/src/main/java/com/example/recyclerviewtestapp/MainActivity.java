30eedc216287ecd63a7d49a9616cb735378a0b66
valentin <delo.biss13@gmail.com>
2022-09-27 19:58:04 +0300

    RecyclerView(project-RecyclerViewTestApp

--- RecyclerViewTestApp/app/src/main/java/com/example/recyclerviewtestapp/MainActivity.java
+++ RecyclerViewTestApp/app/src/main/java/com/example/recyclerviewtestapp/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.recyclerviewtestapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView numbersList;
+    private NumbersAdapter numbersAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numbersList = findViewById(R.id.rv_numbers);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        numbersList.setLayoutManager(layoutManager);
+
+        numbersList.setHasFixedSize(true);
+
+        numbersAdapter=new NumbersAdapter(100);
+        numbersList.setAdapter(numbersAdapter);
     }
 }
