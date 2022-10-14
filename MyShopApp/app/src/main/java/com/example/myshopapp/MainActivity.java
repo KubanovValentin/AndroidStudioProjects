df901fd4dd16b0167103cbe6b18412f3de6aa3a8
valentin <delo.biss13@gmail.com>
2022-10-14 21:14:55 +0300

    MyShopApp part 2

--- MyShopApp/app/src/main/java/com/example/myshopapp/MainActivity.java
+++ MyShopApp/app/src/main/java/com/example/myshopapp/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.myshopapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button joinButton;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        joinButton=(Button)findViewById(R.id.main_join_btn);
+        loginButton=(Button)findViewById(R.id.main_login_btn);
     }
 }
+
+
